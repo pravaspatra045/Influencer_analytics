955df8f37a092b4ba9744c1eb02f5df6646fca25
Pravaspatra <pravas.patra@klockmate.com>
2026-06-25 20:06:21 +0530

    Phase 8 completed - Redis & Celery integration

--- config/settings/base.py
+++ config/settings/base.py
@@ -100,8 +100,20 @@ REST_FRAMEWORK.update({
 
 # celery config
 CELERY_BROKER_URL = "redis://redis:6379/0"
+
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
+
 CELERY_ACCEPT_CONTENT = ["json"]
+
 CELERY_TASK_SERIALIZER = "json"
+
+CELERY_RESULT_SERIALIZER = "json"
+
+CELERY_TIMEZONE = "Asia/Kolkata"
+
+CELERY_TASK_TRACK_STARTED = True
+
+CELERY_TASK_TIME_LIMIT = 30 * 60
 
 #AWS settings
 import boto3
